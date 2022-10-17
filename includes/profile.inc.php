d455ef1416caa34e5d984a2431d00914220892dc
Norman Yap <normanyaptw@yahoo.com>
2022-10-17 16:41:53 +0800

    volunteer Profile

---
+++ includes/profile.inc.php
@@ -0,0 +1,61 @@
+<?php
+require 'dbh.inc.php';
+//THIS file can be used by BOTH VOLUNTEER AND SCHOOL ADMIN
+
+// this variable will differ depending if a user is a volunteer or school admin
+$userType;
+$data = array();
+
+// variables for volunteer
+$fullName;
+$birthdate;
+$occupation;
+$email;
+$phoneNumber;
+$age;
+
+// variables for school admin
+
+
+// check if username is in the volunteer table 
+$volunteerQuery = "SELECT * FROM volunteer WHERE username = \"Norman\"";
+$volunteerResult = mysqli_query($conn, $volunteerQuery);
+
+
+
+if (mysqli_num_rows($volunteerResult) > 0) {
+    while($row = mysqli_fetch_assoc($volunteerResult)) {
+        $data[] = $row;
+    }
+
+    foreach($data as $v){
+        $fullName = $v['fullName'];
+        $birthdate = $v['dateOfBirth'];
+        $occupation = $v['occupation'];
+        $email = $v['email'];
+        $phoneNumber = $v['phoneNumber'];
+    }
+
+    // get system date
+    $systemDate = date('Y-m-d');
+    $systemDate = explode('-', $systemDate);
+    $systemYear = $systemDate[0];
+   
+
+    $birthdateArray = date($birthdate);
+    $birthdateArray = explode('-', $birthdate);
+    $birthYear = $birthdateArray[0];
+
+    $age = $systemDate[0] - $birthdateArray[0];
+    
+    $userType = "Volunteer 💜";
+}
+
+
+
+
+//else check if the username is in the school admin table
+
+
+
+
