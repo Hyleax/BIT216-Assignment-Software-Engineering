0f45ee2f82d77c1611c38448530d8b7c99a615ab
Norman Yap <normanyaptw@yahoo.com>
2022-10-17 16:38:12 +0800

    added change password for volunteer

--- includes/changePassword.inc.php
+++ includes/changePassword.inc.php
@@ -1,4 +1,49 @@
 <?php
+session_start();
 
-// check if user accessed changePassword.inc.php through natural means
-if (isset($_POST['']))
+// check if user accessed this file by regular means
+if (isset($_POST['changePassword-btn'])){
+
+    // require is used when the file is mandatory for the application
+    require 'dbh.inc.php';
+    require 'volunteerFunctions.inc.php';
+
+    // collecting the variables from registerVolunteer.php
+    $oldPassword = $_POST['oldPassword'];
+    $newPassword = $_POST['newPassword'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    
+    // check if fields are empty
+    if (checkPasswordFieldsEmpty($oldPassword, $newPassword, $confirmPassword)){
+        header("Location: ../changePassword.php?error=emptyfields");
+        exit();
+    }
+
+     // check if password is equal to users password in the DB
+    if (checkPasswordInDB($conn, $oldPassword)){
+        header("Location: ../changePassword.php?error=passwordwrong");
+        exit();
+    }   
+
+     // check if the new password is too weak
+    if (testPasswordStrength($newPassword)){
+        header("Location: ../changePassword.php?error=passwordtooweak");
+        exit();
+    }
+
+     // check if the newPassword and confirmPasword match
+     if (confirmPassword($newPassword, $confirmPassword)){
+        header("Location: ../changePassword.php?error=passwordnotequal");
+        exit();
+    }
+
+     //change Password
+     changePassword($conn, $newPassword);
+}
+
+else {
+    header("Location: ../editVolunteerProfile.php");
+}
+
+?>
