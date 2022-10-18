17f5b08de094d283cfbe9ed6785381a9beafe426
loong1998 <81916572+loong1998@users.noreply.github.com>
2022-10-18 09:25:17 +0800

    Added school administrator variables

--- includes/profile.inc.php
+++ includes/profile.inc.php
@@ -15,12 +15,20 @@ $phoneNumber;
 $age;
 
 // variables for school admin
-
+$fullnameSA;
+$position;
+$emailSA;
+$phoneNumSA;
+$staffID;
+$schoolID;
 
 // check if username is in the volunteer table 
 $volunteerQuery = "SELECT * FROM volunteer WHERE username = '".$_SESSION['username']."'";
 $volunteerResult = mysqli_query($con, $volunteerQuery);
 
+// check if username is in the school administrator table 
+$schoolAdminQuery = "SELECT * FROM schooladministrator WHERE username='".$_SESSION['username']."';";
+$schoolAdminResult = mysqli_query($con, $schoolAdminQuery);
 
 
 if (mysqli_num_rows($volunteerResult) > 0) {
@@ -51,11 +59,26 @@ if (mysqli_num_rows($volunteerResult) > 0) {
     $userType = "Volunteer 💜";
 }
 
-
-
-
 //else check if the username is in the school admin table
+else if(mysqli_num_rows($schoolAdminResult) > 0){
+    while($row = mysqli_fetch_assoc($schoolAdminResult)){
+        $data[] = $row;
+    }
 
+    foreach($data as $sa){
+        $fullnameSA = $sa["fullname"];
+        $position = $sa["position"];
+        $emailSA = $sa["email"];
+        $phoneNumSA = $sa["phoneNum"];
+        $staffID = $sa["staffID"];
+        $schoolID = $sa["schoolID"];
+    }
 
-
-
+    if($position <> "Super Admin"){
+        $userType = "School Admin 🧡";
+    }
+    else if($fullnameSA === "Super Admin"){
+        $userType = "Super Admin 💙";
+    }
+    
+}
