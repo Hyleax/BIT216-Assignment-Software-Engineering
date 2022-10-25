54d6a335a4f345799ad9167cc828c6ff26177327
Norman Yap <normanyaptw@yahoo.com>
2022-10-25 22:00:41 +0800

    modify submitRequest.php

--- submitRequest.php
+++ submitRequest.php
@@ -38,7 +38,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="submitNewRequest.html">Submit New Request</a>
+                    <a class="nav-link" href="submitRequest.php">Submit New Request</a>
                 </li>
 
                 <li class="nav-item">
